1da640b90a90ee972c23c4dd7ad1e8146af6a55e
PeterMM <peter5maksimov57@gmail.com>
2026-07-22 01:30:35 +0300

    Com 1.2

--- main/views.py
+++ main/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
   
 def index(request):
-    return HttpResponse("Apl_for_bg")
+    return render(request, "index.html")
+    # return HttpResponse("Apl_for_bg")
+
+def about(request):
+    return HttpResponse("О сайте")
+ 
+def contact(request):
+    return HttpResponse("Контакты")
