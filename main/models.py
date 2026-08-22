20b0be140d566f1a8e7f7a110c7f2b6e6b2ffe34
PeterMM <peter5maksimov57@gmail.com>
2026-08-22 22:45:53 +0300

    2.4 Заготовки моделей транзакций без обновления миграций

--- main/models.py
+++ main/models.py
@@ -7,3 +7,36 @@ class Person(models.Model):
     password = models.CharField(max_length=20)
     balance = models.FloatField()
     role = models.CharField()
+
+
+    @property
+    def balance(self):
+        return self._balance
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Transaction(models.Model):
+    user_id = models.IntegerField()
+    amount = models.FloatField()
+    type_tr = models.CharField()
+    time = models.DateTimeField()
+    category = models.CharField()
+    res_or_sen = models.CharField()
+    regullar = models.BooleanField()
+
+
+    @property
+    def categories(self):
+        categories = ["Переводы людям", "Зарплата", "Продукты", "Электроника",
+            "Развлечения", "Кафе и рестораны", "Товары для дома", "АЗС", "Цифровые сервисы",
+            "Пополнение наличными", "Одежда и обувь", "ЖКХ", "Другое"]
+        return categories
+
+
+    def __str__(self):
+        return f'{self.type_tr} суммы {self.amount} пользователя {self.user_id}' 
+
